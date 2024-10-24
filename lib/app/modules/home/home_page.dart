362381d8e35b783f8f65c550a5e0b892bec361e9
João Pedro <devjpdesenvolver@gmail.com>
2024-10-24 09:56:50 -0300

    feat: add CardTcgWidget

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,9 +1,8 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:gengar_dex/app/modules/home/store/listing_store.dart';
+import 'package:gengar_dex/app/widgets/card_tcg_widget.dart';
 import 'package:gengar_dex/sdk/Models/card_tcg_brief.dart';
-import 'package:shimmer/shimmer.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -41,59 +40,9 @@ class _HomePageState extends State<HomePage> {
                 }
                 return Row(
                   children: [
-                    SizedBox(
-                      width: MediaQuery.sizeOf(context).width / 2 - 5,
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            height: 200,
-                            width: MediaQuery.sizeOf(context).width / 2 - 5,
-                            child: CachedNetworkImage(
-                              imageUrl: '${card1.image}/low.webp',
-                              placeholder: (context, url) => Shimmer.fromColors(
-                                baseColor: Colors.transparent,
-                                highlightColor: Colors.black12,
-                                child: Container(
-                                  color: Colors.grey[300]!,
-                                ),
-                              ),
-                              errorWidget: (context, url, error) =>
-                                  const Icon(Icons.error),
-                            ),
-                          ),
-                          Text(card1.name ?? ''),
-                          Text(card1.id ?? ''),
-                          Text(card1.localId ?? ''),
-                        ],
-                      ),
-                    ),
+                    CardTcgWidget(cardTCGBrief: card1),
                     const SizedBox(width: 10),
-                    SizedBox(
-                      width: MediaQuery.sizeOf(context).width / 2 - 5,
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            height: 200,
-                            width: MediaQuery.sizeOf(context).width / 2 - 5,
-                            child: CachedNetworkImage(
-                              imageUrl: '${card2.image}/low.webp',
-                              placeholder: (context, url) => Shimmer.fromColors(
-                                baseColor: Colors.transparent,
-                                highlightColor: Colors.black12,
-                                child: Container(
-                                  color: Colors.grey[300]!,
-                                ),
-                              ),
-                              errorWidget: (context, url, error) =>
-                                  const Icon(Icons.error),
-                            ),
-                          ),
-                          Text(card2.name ?? ''),
-                          Text(card2.id ?? ''),
-                          Text(card2.localId ?? ''),
-                        ],
-                      ),
-                    ),
+                    CardTcgWidget(cardTCGBrief: card2),
                   ],
                 );
               }),
--- lib/app/widgets/card_tcg_widget.dart
+++ lib/app/widgets/card_tcg_widget.dart
@@ -1,10 +1,38 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:gengar_dex/sdk/Models/card_tcg_brief.dart';
+import 'package:shimmer/shimmer.dart';
 
 class CardTcgWidget extends StatelessWidget {
-  const CardTcgWidget({super.key});
+  const CardTcgWidget({super.key, required this.cardTCGBrief});
+  final CardTCGBrief cardTCGBrief;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width / 2 - 5,
+      child: Column(
+        children: [
+          SizedBox(
+            height: 200,
+            width: MediaQuery.sizeOf(context).width / 2 - 5,
+            child: CachedNetworkImage(
+              imageUrl: '${cardTCGBrief.image}/high.webp',
+              placeholder: (context, url) => Shimmer.fromColors(
+                baseColor: Colors.transparent,
+                highlightColor: Colors.black12,
+                child: Container(
+                  color: Colors.grey[300]!,
+                ),
+              ),
+              errorWidget: (context, url, error) => const Icon(Icons.error),
+            ),
+          ),
+          Text(cardTCGBrief.name ?? ''),
+          Text(cardTCGBrief.id ?? ''),
+          Text(cardTCGBrief.localId ?? ''),
+        ],
+      ),
+    );
   }
 }
