7e206907786e3fcc7484fa0c64610ec51c7e85f3
João Pedro <devjpdesenvolver@gmail.com>
2024-11-04 12:00:25 -0300

    fix(test): fix test based on sets in list of cards

--- test/app/modules/home/store/listing_store_test.dart
+++ test/app/modules/home/store/listing_store_test.dart
@@ -6,6 +6,9 @@ void main() {
   test('Test ListingStore.init', () async {
     await store.init();
     expect(store.listCards.isNotEmpty, equals(true));
-    expect(store.listCards.first.id, equals('sv06-001'));
+    expect(store.listSets.isNotEmpty, equals(true));
+    expect(store.listSets.first.id, equals('base1'));
+    expect(store.listCards.first.id?.contains(store.listSets.last.id),
+        equals(true));
   });
 }
