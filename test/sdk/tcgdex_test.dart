85f17d985bb5925cac2ccf8bbbee3e39329462d7
João Pedro <devjpdesenvolver@gmail.com>
2024-11-12 17:55:03 -0300

    test(sdk): test search cards

--- test/sdk/tcgdex_test.dart
+++ test/sdk/tcgdex_test.dart
@@ -1,4 +1,5 @@
 import 'package:gengar_dex/sdk/Models/card_tcg.dart';
+import 'package:gengar_dex/sdk/Models/card_tcg_brief.dart';
 import 'package:gengar_dex/sdk/Models/set_tcg.dart';
 import 'package:gengar_dex/sdk/tcgdex.dart';
 import 'package:test/test.dart';
@@ -6,7 +7,7 @@ import 'package:test/test.dart';
 void main() {
   TCGDex sdk = TCGDex();
   test('Test sdk.getSetCards', () async {
-    List set = await sdk.getSetCards('sv05');
+    List<CardTCGBrief> set = await sdk.getSetCards('sv05');
     expect(set.isNotEmpty, equals(true));
     expect(set.first.id, equals('sv05-001'));
   });
@@ -21,5 +22,19 @@ void main() {
     List<SetTcg> sets = await sdk.getSets();
     expect(sets.first.id, equals('base1'));
     expect(sets.any((set) => set.name == 'Coroa Estelar'), equals(true));
+  });
+
+  group('Tests for search', () {
+    test('Test sdk.searchCards for a valid search', () async {
+      List<CardTCGBrief> cards = await sdk.searchCards('pikachu');
+      expect(cards.isNotEmpty, equals(true));
+      expect(cards.first.name?.contains('Pikachu'), true);
+    });
+
+    test('Test sdk.searchCards returns empty list for invalid search',
+        () async {
+      List<CardTCGBrief> cards = await sdk.searchCards('InvalidCardName');
+      expect(cards.isEmpty, equals(true));
+    });
   });
 }
